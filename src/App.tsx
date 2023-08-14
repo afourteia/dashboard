f27b021638ed3c18ff9a8e09871a3fa329754b82
Abdul <raoph40@gmail.com>
2023-08-13 18:22:43 -0600

    Added navigation tab

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useMemo } from 'react'
 import { themeSettings } from '@/theme'
 import { Box, CssBaseline, ThemeProvider } from '@mui/material'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from '@/scenes/Navbar'
+import Navbar from '@/scenes/navbar'
 
 function App() {
   const theme = useMemo(() => {
@@ -19,7 +19,11 @@ function App() {
             <Box width='100%' height='100%' padding='1rem 2rem 4rem 2rem'>
               <Navbar />
               <Routes>
-                <Route path='/' element={<div>Dashboard page</div>} />
+                <Route
+                  path='/'
+                  element={<div>Dashboard----------------- page</div>}
+                />
+                <Route path='/dashboard' element={<div>dashboard page</div>} />
                 <Route
                   path='/predictions'
                   element={<div>Predictions page</div>}
